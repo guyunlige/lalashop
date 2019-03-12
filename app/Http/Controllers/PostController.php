7f3d8f653e050446593ae5c47cc2d1475e7036ee
guyunlige <2543853429@qq.com>
2019-03-12 22:55:31 +0800

    layout.blade.php

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -11,7 +11,7 @@ class PostController extends Controller
     public function index()
     {
 //        $posts = Post::orderBy('created_at','desc')->get();
-//        return view('post/index',compact($posts));
+//        return view('post/index',compact(posts));
         return view('post/index');
 
     }
